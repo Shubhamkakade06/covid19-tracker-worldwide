83716f15b59f74dc439832b62d3a6b3dc9bed16a
Shubham <shubhamkakade061@gmail.com>
2020-07-28 19:18:51 +0530

    Some UI Changes

---
+++ src/main/resources/static/js/covidTable.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () {
+    //alert('hello');
+    $('#covidDataTable').DataTable({
+        "pagingType": "full_numbers",
+        "responsive": "true",
+        //"pagingType": "simple", // "simple" option for 'Previous' and 'Next' buttons only
+        "order": [[ 0, 'asc' ]]
+    });
+    $('.dataTables_length').addClass('bs-select');
+});
